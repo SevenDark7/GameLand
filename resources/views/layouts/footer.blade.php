fe5aec8841f3db7989cfab807834bebfb56965b4
SevenDark7 <abedim308@gmail.com>
2021-09-12 01:39:23 +0430

    add footer for pages

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,44 @@
+<footer>
+    <div class="left">
+        <div>
+            <h4>ارتباط با ما</h4>
+        </div>
+        <div>
+            <div class="mail">
+                <a href="#">SevenEncoder@gmail.com :ایمیل</a>
+                <i class="material-icons">email</i>
+            </div>
+            <div class="telegram">
+                <a href="#">@SevenDark :تلگرام</a>
+                <i class="material-icons">near_me</i>
+            </div>
+        </div>
+    </div>
+    <div class="middle">
+        <div>
+            <h4>بخش های سایت</h4>
+        </div>
+        <div>
+            <a href="#">قوانین و مقررات</a>
+            <a href="#">درباره گیم شاپ</a>
+            <a href="#">ارتباط با ما</a>
+        </div>
+        <div>
+            <img src="/images/trust.png" alt="Symbol Of Trust">
+        </div>
+    </div>
+    <div class="right">
+        <div>
+            <h4>درباره سایت</h4>
+        </div>
+        <div>
+            <p>گیم شاپ یکی از کاملترین و بروزترین وبسایت ها در زمینه گیمینگ و انواع بازی های ویدیویی میباشد
+                که در سدد است همیشه جدیدترین و بروزترین ترین مقالات و اخبار مربوط به بازی ها را در اختیار علاقمندان
+                قرار دهد و آن ها را از انواع بازی ها مطلع سازد
+            </p>
+        </div>
+
+    </div>
+
+
+</footer>
