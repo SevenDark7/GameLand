7820c1875ab2ed05e4210c96dbeaae1a6db6bde6
SevenDark7 <abedim308@gmail.com>
2021-09-16 22:26:55 +0430

    add link to profile, favorites and gameslist for dives

--- public/js/master.js
+++ public/js/master.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
+    $('.select > div').animate({margin: '7vw 3vw', opacity: '1'}, 'slow');
     $('.sldMenu').click(function(e) {
         e.preventDefault();
         $('.side').slideToggle();
     });
+});
+
+$('.list').click(function() {
+    $(location).attr('href', '/games');
+});
+
+$('.favorites').click(function() {
+    $(location).attr('href', '/favorites');
+});
+
+$('.profile').click(function() {
+    $(location).attr('href', '/profile');
 });
