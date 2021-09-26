6d2c56c4800dcae7b2b3958d6b3caf35d8c6d615
SevenDark7 <abedim308@gmail.com>
2021-09-27 00:12:20 +0330

    add routes and pull data from database for admin panel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Game;
+use Illuminate\Support\Facades\Validator;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,8 +32,51 @@ Route::get('/favorites', function () {
 });
 
 Route::get('/games', function () {
-    $games = DB::table('games')->get();
+    $games = Game::orderBy('ID')->get();
     return view('gamesList', [
         'games' => $games
     ]);
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return view('admin.admin');
+    });
+
+    Route::get('/comments', function () {
+        return view('articles.comments');
+    });
+
+    Route::get('/register', function () {
+        $games = Game::all();
+        return view('admin.articles.create', [
+            'games' => $games
+        ]);
+    });
+
+    Route::post('/articles/create', function () {
+        $validator = Validator::make(request()->all(), [
+            'name' => 'required',
+            'release' => 'required',
+            'publish' => 'required',
+            'genre' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator);
+        }
+        $release = array();
+        $release = explode('/', request('release'));
+        strlen($release[0]) < 2 ? $release[0] = "0" . $release[0] : $release[0];
+        strlen($release[1]) < 2 ? $release[1] = "0" . $release[1] : $release[1];
+        $finalRelease = implode('/', $release);
+        Game::create([
+            'Name' => ucwords(request('name')),
+            'Slug' => implode('+', explode(' ', request('name'))),
+            'Platform' => strtoupper(request('platform')),
+            'Release' => $finalRelease,
+            'Publisher' => ucwords(request('publish')),
+            'Genre' => ucwords(request('genre'))
+        ]);
+        return redirect('admin/register');
+    });
 });
