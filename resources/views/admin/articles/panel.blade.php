8ffd2b11fe20f2bc9539fdf2036f49c312680e20
SevenDark7 <abedim308@gmail.com>
2021-09-30 01:32:30 +0330

    add create and update for games

--- resources/views/admin/articles/panel.blade.php
+++ resources/views/admin/articles/panel.blade.php
@@ -10,8 +10,8 @@
             <li class="drop-down text-light mb-3">
                 <button type="button" class="btn dropdown-toggle" data-toggle="dropdown">Home</button>
                 <ul class="dropdown-menu list-unstyled fw-normal" style="cursor: pointer">
-                    <li class="dropdown-item">Delete Game</li>
                     <li class="dropdown-item">Update Game</li>
+                    <li class="dropdown-item">Delete Game</li>
                 </ul>
             </li>
             <li class="drop-down text-light mb-3">
@@ -31,12 +31,124 @@
                 <li class="dropdown-item text-danger">Sign Out</li>
             </ul>
         </div>
-
     </section>
     <section class="d-flex flex-column bg-dark" style="flex-basis: 40%">
-        <div class="d-flex justify-content-center align-items-center py-3 border-bottom mb-3">
+        <div class="d-flex justify-content-center align-items-center py-3 border-bottom">
             <i class="material-icons mr-3 text-primary" style="font-size: 4em">apps</i>
             <h3 class="text-light">Quick Access</h3>
         </div>
+        @if($errors->any())
+            <div class="alert alert-danger">
+                <ul class="text-right" dir="auto">
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <ul class="list-group list-unstyled" style="overflow-y: auto">
+            <li class="list-group-item bg-dark border-white rounded-0 border-top-0 border-left-0">
+                <form action="/admin/articles/register" method="post">
+                    @csrf
+                    <legend class="text-info">Insert Game</legend>
+                    <table class="table table-dark table-sm">
+                        <tbody>
+                            <tr>
+                                <td><label for="name" class="text-light">Game Name</label></td>
+                                <td><input type="text" name="name" class="form-control" autocomplete="off" placeholder="Enter The Name Of The Game"></td>
+                            </tr>
+                            <tr>
+                                <td><label for="platform" class="text-light">Game Platform</label></td>
+                                <td><input type="text" name="platform" class="form-control" autocomplete="off" placeholder="Enter The Game Platform"></td>
+                            </tr>
+                            <tr>
+                                <td><label for="release" class="text-light">Game Release</label></td>
+                                <td><input type="date" name="release" class="form-control" autocomplete="off" placeholder="Enter The Game Release"></td>
+                            </tr>
+                            <tr>
+                                <td><label for="publish" class="text-light">Game Publisher</label></td>
+                                <td><input type="text" name="publish" class="form-control" autocomplete="off" placeholder="Enter The Game Publisher"></td>
+                            </tr>
+                            <tr>
+                                <td><label for="genre" class="text-light">Game Genre</label></td>
+                                <td><input type="text" name="genre" class="form-control" autocomplete="off" placeholder="Specify The Genre Of The Game"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div class="d-flex justify-content-around">
+                        <button type="submit" class="btn btn-sm w-25 btn-outline-success">Register</button>
+                        <button type="reset" class="btn btn-sm w-25 btn-outline-danger">Cancel</button>
+                    </div>
+                </form>
+            </li>
+            <li class="list-group-item bg-dark border-white rounded-0 border-left-0">
+                @if(isset($notFind))
+                    <div class="alert alert-danger">
+                        <ul class="text-right" dir="auto">
+                            <li>بازی با این نام یافت نشد</li>
+                        </ul>
+                    </div>
+                @endif
+                <form action="/admin/articles/update" method="post">
+                    @csrf
+                    <legend class="text-info">Update Game</legend>
+                    <div class="form-group">
+                        <label for="name" class="text-light">Game Name</label>
+                        <input type="text" name="name" class="form-control" autocomplete="off" placeholder="Enter The Name Of The Game To Edit">
+                    </div>
+                    <div class="d-flex justify-content-around">
+                        <button type="submit" class="btn btn-sm w-25 btn-outline-success">Check</button>
+                        <button type="reset" class="btn btn-sm w-25 btn-outline-danger">Cancel</button>
+                    </div>
+                </form>
+
+                @if(isset($finded))
+                    <form action="/admin/articles/updated" method="post">
+                        @csrf
+                        <table class="table table-dark table-sm mt-3">
+                            <tbody>
+                            <tr>
+                                <td><label for="New" class="text-light">Edited Name</label></td>
+                                <td><input type="text" name="updatedName" class="form-control" value="{{ $finded->id }}" autocomplete="off" placeholder="Enter The Edited Name Of The Game"></td>
+                            </tr>
+                            <tr>
+                                <td><label for="New" class="text-light">Edited Platform</label></td>
+                                <td><input type="text" name="updatedPlatform" class="form-control" value="{{ $finded->platform }}" autocomplete="off" placeholder="Enter The Game Edited Platform"></td>
+                            </tr>
+                            <tr>
+                                <td><label for="New" class="text-light">Edited Release</label></td>
+                                <td><input type="text" name="updatedRelease" class="form-control" value="{{ $finded->release }}" autocomplete="off" placeholder="Enter The Edited Release Date Of The Game"></td>
+                            </tr>
+                            <tr>
+                                <td><label for="New" class="text-light">Edited Publisher</label></td>
+                                <td><input type="text" name="updatedPublisher" class="form-control" value="{{ $finded->publisher }}" autocomplete="off" placeholder="Enter The Edited Game Publisher"></td>
+                            </tr>
+                            <tr>
+                                <td><label for="New" class="text-light">Edited Genre</label></td>
+                                <td><input type="text" name="updatedGenre" class="form-control" value="{{ $finded->genre }}" autocomplete="off" placeholder="Specify The Edited Genre Of The Game"></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <div class="d-flex justify-content-around">
+                            <button type="submit" class="btn btn-sm w-25 btn-outline-success">Edit</button>
+                            <button type="reset" class="btn btn-sm w-25 btn-outline-danger">Cancel</button>
+                        </div>
+                    </form>
+                @endif
+            </li>
+            <li class="list-group-item bg-dark border-white rounded-0 border-left-0">
+                <form href="/admin/articles/delete" method="post">
+                    <legend class="text-info">Delete Game</legend>
+                    <div class="form-group">
+                        <label for="New" class="text-light">Game Name</label>
+                        <input type="text" name="delete" class="form-control" autocomplete="off" placeholder="Enter The Name Of The Game To Delete">
+                    </div>
+                    <div class="d-flex justify-content-around">
+                        <button type="submit" class="btn btn-sm w-25 btn-outline-success">Delete</button>
+                        <button type="reset" class="btn btn-sm w-25 btn-outline-danger">Cancel</button>
+                    </div>
+                </form>
+            </li>
+        </ul>
     </section>
 @endsection
