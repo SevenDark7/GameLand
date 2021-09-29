9fab29a7100c7623e9e8ff3a840fc3c793ef9d91
SevenDark7 <abedim308@gmail.com>
2021-09-30 01:31:26 +0330

    change admin panel mvc

---
+++ resources/views/admin/articles/layouts/leftSide.blade.php
@@ -0,0 +1,24 @@
+<section class="bg-dark d-flex flex-column mr-3" style="flex-basis: 25%">
+    <div class="d-flex justify-content-center align-items-center py-3 border-bottom mb-3">
+        <i class="material-icons mr-3 text-primary" style="font-size: 4em">account_circle</i>
+        <h3 class="text-light">Admin Panel</h3>
+    </div>
+    <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item d-flex mb-4 pl-5 text-light">
+            <i class="material-icons px-3">home</i>
+            <a href="/register" class="text-light">Home</a>
+        </li>
+        <li class="nav-item d-flex mb-4 pl-5 text-light">
+            <i class="material-icons px-3">dashboard</i>
+            <a href="/dashboard" class="text-light">Dashboard</a>
+        </li>
+        <li class="nav-item d-flex mb-4 pl-5 text-light">
+            <i class="material-icons px-3">list</i>
+            <a href="/list" class="text-light">Gams</a>
+        </li>
+    </ul>
+    <hr>
+    <div class="dropdown d-flex justify-content-start pl-5 mb-3">
+        <a href="/" class="text-danger px-3">Sign Out</a>
+    </div>
+</section>
